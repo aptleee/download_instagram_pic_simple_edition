257c1c77bb46b885145481eba1586643bfa02e6e
xlapt <viclx1223@gmail.com>
2018-11-26 23:24:00 -0500

    can run

---
+++ getimage.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/zsh
+wget $1 -O ~/i.txt
+#url=`grep -E -o '(https:)(.|/|\w)*?\.(?:jpg|gif|png)' ~/i.txt | head -1`
+grep -E -o '(https:)(.|/|\w)*?\.(jpg|gif|png)' ~/i.txt >> url1.txt
+sort -u url1.txt | uniq >> url2.txt
+grep -E -o '(https:)(.|/|\w)*?(-\d{2}/e35)(.|/|\w)*?\.(jpg|gif|png)' url2.txt >> url3.txt
+#wget $url
+for url in `cat url3.txt`
+do
+	wget $url
+done
+rm -f ~/i.txt url1.txt url2.txt url3.txt
