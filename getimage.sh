14d5e2e7a6bb0d906e6c8f065986536233413709
xlapt <viclx1223@gmail.com>
2018-11-27 00:14:55 -0500

    some revision

--- getimage.sh
+++ getimage.sh
@@ -7,6 +7,6 @@ grep -E -o '(https:)(.|/|\w)*?(-\d{2}/e35)(.|/|\w)*?\.(jpg|gif|png)' url2.txt >>
 #wget $url
 for url in `cat url3.txt`
 do
-	wget $url
+	wget $url -O ~/insImage/`openssl rand -base64 8`.${url:(-3)}
 done
 rm -f ~/i.txt url1.txt url2.txt url3.txt
